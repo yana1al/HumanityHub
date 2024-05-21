ece9284bc0dc6c611dee18f893e3fbd0eb2f95ce
yana1al <alimasharma@gmail.com>
2024-05-21 11:28:14 -0700

    fetch events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -23,7 +23,7 @@ const Events = () => {
   const handleSubmitSearchEvents = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get("https://humanity-hub1-3599a88da879.herokuapp.com/api/events", {
+      const response = await axios.get("https://humanity-hub-back-0e67c67407b5.herokuapp.com/api/events", {
         params: searchQuery
       });
 
