5685f0324b54773b31719d6a86ff4e3ced1b0f13
yana1al <alimasharma@gmail.com>
2024-05-21 00:12:34 -0700

    reviews

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+
 
 const Home = ({ user, isAdmin }) => {
   console.log(user);
